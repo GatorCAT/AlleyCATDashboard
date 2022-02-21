ce0db573318f7d1bb429401d1caf601922ebf2f1
Wesley Long <46755903+WesleyL30@users.noreply.github.com>
2022-02-20 22:32:55 -0500

    Add Bar graph functionality

--- GatorCATDashboard/pymongostream.py
+++ GatorCATDashboard/pymongostream.py
@@ -1,47 +1,74 @@
 # streamlit_app.py
 import os
+import plotly.express as px
+import pandas as pd
 import streamlit as st
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
 load_dotenv()
 # Initialize connection.
-mongopass = os.getenv("mongopassword")
+username = os.getenv("username")
+mongopass = os.getenv("password")
+cluster_name = os.getenv("cluster_name")
+collection_name = os.getenv("collection_name")
 #print(mongopass)
-client = MongoClient(f"mongodb+srv://admin:{mongopass}@streamlit-test.u4cyq.mongodb.net/test-game?retryWrites=true&w=majority")
+client = MongoClient(f"mongodb+srv://admin:{mongopass}@{cluster_name}.u4cyq.mongodb.net/{collection_name}?retryWrites=true&w=majority")
 db = client.test
-db = client.get_database("test-game")
+db = client.get_database(collection_name)
 # Pull data from the collection.
 # Uses st.cache to only rerun when the query changes or after 10 min.
 @st.cache(ttl=600)
-def get_data():
-    items = db.player.distinct("name")
+def get_class():
+    items = db.StudentData.distinct("class")
     items = list(items)  # make hashable for st.cache
-    #print(items)
     return items
 
 @st.cache(ttl=600)
-def get_person(player_name):
-    db = client.get_database("test-game")
-    output = db.player.find({"name": player_name})
+def get_assignments(class_name):
+    output = db.StudentData.find({"class": class_name})
     output = list(output)
     return output
 
-def get_class(chsn_class):
-    results = db.player.find({"name":chsn_class},{"_id":0,"class":1})
-    choices = get_assignments(results)
+def get_assignment(chosen_class):
+    results = db.StudentData.find({"class":chosen_class},{"_id":0,"assignment":1})
+    choices = get_unique_assignments(results)
     return choices
 
-def get_assignments(items):
+def get_unique_assignments(items):
     output = set()
     for entry in items:
-        output.add(entry["class"])
+        output.add(entry["assignment"])
     return list(output)
 
-if "class" not in st.session_state:
-    st.session_state["class"] = get_data()
+def get_bar_data(org, assignment):
+    
+    output = db.StudentData.find({"class": org, "assignment": assignment},{"_id": 0, "checks": 1})
+    output = [document["checks"] for document in output]
+    df = pd.DataFrame(output)
+    total = df.sum(axis=0)
+    return (total/len(output))
 
-st.session_state["name"] = st.selectbox("Select a name", st.session_state["class"])
-st.session_state["other"] = st.selectbox("Select a class", get_class(st.session_state["name"]))
-if st.session_state["name"] and st.session_state["other"]:
-    st.write(get_person(st.session_state["other"]))
+def main():
+    if "classes" not in st.session_state:
+        st.session_state["classes"] = get_class()
+
+    st.session_state["class"] = st.sidebar.selectbox("Select a class", st.session_state["classes"],index=0)
+    st.session_state["assignments"] = st.sidebar.selectbox("Select a assignment", get_assignment(st.session_state["class"]),index=0)
+    st.session_state["Type"] = st.selectbox("Select a graph", ["Bar", "Line"])
+    data = get_bar_data(st.session_state["class"], st.session_state["assignments"])
+    if st.session_state["Type"] == "Line":
+        st.line_chart(data)
+    elif st.session_state["Type"] == "Bar":
+        switch = st.checkbox("Switch Orientation")
+        if switch:
+            st.session_state["Orientation"] = "h"
+        else:
+            st.session_state["Orientation"] = ""
+        fig = px.bar(data, labels={"index": "Checks", "value": "Average Checks Completed"},width=800,height=1000,orientation=st.session_state["Orientation"])
+        fig.update_layout(showlegend=False)
+        fig.update_traces(width=.5)
+    st.plotly_chart(fig)
+
+if __name__ == "__main__":
+    main()
